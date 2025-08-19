a0047b47820ab0fbeac9659b8ad67efc1df2057a
sneed <100238373+a4hmed4@users.noreply.github.com>
2025-08-19 03:03:31 +0300

    Delete huggingface_models_downloader.py

--- huggingface_models_downloader.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import snapshot_download
-
-model_name = "Qwen/Qwen3-4B"
-local_dir = "./models/qwen3-4b"
-
-snapshot_download(
-    repo_id=model_name,
-    local_dir=local_dir,
-)
